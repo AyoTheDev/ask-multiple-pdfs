55f585ee0c3062b587c39cc58428f53888e02cfd
ayoola adedeji <ayoola.adedeji@outlook.com>
2024-02-29 15:53:56 +0000

    updated prompt

--- prompts.py
+++ prompts.py
@@ -2,8 +2,8 @@ prompt_template = """You are an AI assistant specialized in generating executive
 is to create an executive summary.
 
 The executive summary should have:
-- 1 short paragraph that summarise the talent background
-- 1 short paragraph that explains if they are a good match to the job spec
+- 1 short paragraph (2 sentences) that summarise the talent background
+- 1 short paragraph (2 sentences) that explains if they are a good match to the job spec
 - 3-4 bullet points on key skills and experience related to the job spec
 - If available, when they can start the job from, salary expectations, and location/timezone
 The executive summary must be limited to 200 words
